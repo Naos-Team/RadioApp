50e0887f8d631168d532c299dd19e29ae293fc8a
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-09-07 11:07:35 +0700

    nhi cm

--- src/screens/MostViewScreen.js
+++ src/screens/MostViewScreen.js
@@ -1,10 +1,26 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const MostViewScreen = () => {
+  const getMostViewRadio = async (page, step, uid) => {
+    try {
+      const response = await fetch(
+        `http://tuanpc.pw/radiorn/api/radio/getMostViewRadio.php?page=${page}&step=${step}&uid=${uid}`
+      );
+      const json = await response.json();
+      console.warn(json.data)
+      return json.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(()=>{
+    getMostViewRadio(1,5,'usertest2')
+  },[])
   return (
     <View>
-      <Text>MostViewScreen</Text>
+      
+    
     </View>
   )
 }
